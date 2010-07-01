e640c3d00c03320e96decc981e7c924cea41451f
Francois Keith <francois.keith@aist.go.jp>
2010-07-01 18:39:44 +0900

    Remove a header generated by cmake from git repository

--- include/sot-core/import-default-paths.h
+++
@@ -1,8 +0,0 @@
-// -*- c++ -*-
-#ifndef SOT_FACTORY_COMMAND_IMPORT_DEFAULT_PATHS_H
-# define SOT_FACTORY_COMMAND_IMPORT_DEFAULT_PATHS_H
-
-/// Default script path as known by CMake at configure time.
-# define SOT_IMPORT_DEFAULT_PATHS "/home/blue/sot-lib/script"
-
-#endif //! SOT_FACTORY_COMMAND_IMPORT_DEFAULT_PATHS_H
