22c8f60fa149a0bdd68df08bc9e02bd3b7ccadb4
Francois Bleibel <fbleibel@gmail.com>
2010-07-07 17:44:03 +0900

    Made sure pthreads were included. Simplified the unit test test_mailbox that previously *required user input*.

--- unitTesting/tools/test_mailbox.cpp
+++ unitTesting/tools/test_mailbox.cpp
@@ -62,34 +62,6 @@ int main( int argc,char** argv )
 {
   boost::thread th( f );
 
-  try
-    {
-      
-      //sotDEBUG(5) << " Loading..." << endl;
-      PluginLoader pl;
-      //pl.setDirectory( "lib/plugin" );
-      //pl.loadPluginList( "plugin.txt" );
-      //pl.loadPlugins();
-      //sotDEBUG(5) << " Loading done." << endl;
-      
-
-      g_shell.referencePluginLoader( &pl );
-
-      for( int i=1;i<argc;++i )
-	{
-	  std::istringstream script( argv[i] );
-	  cout << "Run "<< argv[i] << endl;
-	  g_shell.cmd( "run",script,cout );
-	}
-
-      g_shell.shell(cin,cout);
-    }
-  catch( ExceptionAbstract& e )
-    {
-      cout << "!!  "  << e <<endl;
-    }
-
-
 
   return 0;
 }
