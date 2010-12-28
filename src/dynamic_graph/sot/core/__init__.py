672bf27fa54ee7e09cd5a47f9c079c0e155da42f
florent <florent@laas.fr>
2010-12-28 21:26:35 +0100

    Add documentation for python bindings.
    	* doc/CMakeLists.txt: new,
    	* doc/sphinx/conf.py.in: new,
    	* doc/sphinx/index.rst.in: new,
    	* src/dynamic_graph/sot/core/__init__.py,
    	* CMakeLists.txt.

---
+++ src/dynamic_graph/sot/core/__init__.py
@@ -0,0 +1,42 @@
+from robot_simu import RobotSimu
+from vector_constant import VectorConstant
+from matrix_constant import MatrixConstant
+from unary_op import RPYToMatrix
+from derivator import Derivator_of_Vector
+from feature_point6d import FeaturePoint6d
+from feature_point6d_relative import FeaturePoint6dRelative
+from feature_generic import FeatureGeneric
+from feature_joint_limits import FeatureJointLimits
+from binary_op import Compose_R_and_T
+from task import Task
+from constraint import Constraint
+from gain_adaptive import GainAdaptive
+from sot import SOT
+
+RobotSimu('')
+VectorConstant('')
+MatrixConstant('')
+RPYToMatrix('')
+Derivator_of_Vector('')
+FeaturePoint6d('')
+FeaturePoint6dRelative('')
+FeatureGeneric('')
+FeatureJointLimits('')
+Compose_R_and_T('')
+Task('')
+Constraint('')
+GainAdaptive('')
+SOT('')
+
+
+
+
+
+
+
+
+
+
+
+
+
