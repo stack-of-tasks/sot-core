ae3db436e8be7de704807fbdebe31300bfc620f2
Mansard <nmansard@laas.fr>
2011-01-28 15:19:58 +0100

    Added all small entities to python initial loading.

--- src/dynamic_graph/sot/core/math_small_entities.py
+++ src/dynamic_graph/sot/core/math_small_entities.py
@@ -7,3 +7,109 @@ from vector_constant import VectorConstant
 VectorConstant('')
 from matrix_constant import MatrixConstant
 MatrixConstant('')
+
+
+from unitary_op import Inverse_of_matrix
+Inverse_of_matrix('')
+from unitary_op import Inverse_of_matrixhomo
+Inverse_of_matrixhomo('')
+from unitary_op import Inverse_of_matrixtwist
+Inverse_of_matrixtwist('')
+from unitary_op import Inverse_of_matrixrotation
+Inverse_of_matrixrotation('')
+from unitary_op import Inverse_of_unitquat
+Inverse_of_unitquat('')
+from unitary_op import Selec_of_vector
+Selec_of_vector('')
+from unitary_op import Selec_of_matrix
+Selec_of_matrix('')
+from unitary_op import Selec_column_of_matrix
+Selec_column_of_matrix('')
+from Unitary_Op Import MatrixHomoToPoseUTheta
+MatrixHomoToPoseUTheta('')
+from Unitary_Op Import SkewSymToVector
+SkewSymToVector('')
+from Unitary_Op Import PoseUThetaToMatrixHomo
+PoseUThetaToMatrixHomo('')
+from Unitary_Op Import MatrixHomoToPoseQuaternion
+MatrixHomoToPoseQuaternion('')
+from Unitary_Op Import MatrixHomoToPoseRollPitchYaw
+MatrixHomoToPoseRollPitchYaw('')
+from Unitary_Op Import PoseRollPitchYawToMatrixHomo
+PoseRollPitchYawToMatrixHomo('')
+from Unitary_Op Import PoseRollPitchYawToPoseUTheta
+PoseRollPitchYawToPoseUTheta('')
+from Unitary_Op Import HomoToMatrix
+HomoToMatrix('')
+from Unitary_Op Import MatrixToHomo
+MatrixToHomo('')
+from Unitary_Op Import HomoToTwist
+HomoToTwist('')
+from Unitary_Op Import HomoToRotation
+HomoToRotation('')
+from Unitary_Op Import RPYToMatrix
+RPYToMatrix('')
+from Unitary_Op Import MatrixToRPY
+MatrixToRPY('')
+from Unitary_Op Import QuaternionToMatrix
+QuaternionToMatrix('')
+from Unitary_Op Import MatrixToQuaternion
+MatrixToQuaternion('')
+from Unitary_Op Import MatrixToUTheta
+MatrixToUTheta('')
+from Unitary_Op Import UThetaToQuaternion
+UThetaToQuaternion('')
+from Unitary_Op Import MatrixDiagonal
+MatrixDiagonal('')
+from Unitary_Op Import DirtyMemory
+DirtyMemory('')
+
+
+from binary_op import Add_of_matrix
+Add_of_matrix('')
+from binary_op import Add_of_double
+Add_of_double('')
+from binary_op import Multiply_of_vector
+Multiply_of_vector('')
+from binary_op import Multiply_of_matrix
+Multiply_of_matrix('')
+from binary_op import Multiply_of_matrixhomo
+Multiply_of_matrixhomo('')
+from binary_op import Multiply_of_matrixrotation
+Multiply_of_matrixrotation('')
+from binary_op import Multiply_of_matrixtwist
+Multiply_of_matrixtwist('')
+from binary_op import Multiply_of_quaternion
+Multiply_of_quaternion('')
+from binary_op import Multiply_of_double
+Multiply_of_double('')
+from binary_op import Substract_of_vector
+Substract_of_vector('')
+from binary_op import Substract_of_matrix
+Substract_of_matrix('')
+from binary_op import Substract_of_double
+Substract_of_double('')
+from binary_op import Stack_of_vector
+Stack_of_vector('')
+from binary_op import WeightAdd_of_vector
+WeightAdd_of_vector('')
+from Binary_Op Import WeightDir
+WeightDir('')
+from Binary_Op Import Nullificator
+Nullificator('')
+from binary_op import Compose_R_and_T
+Compose_R_and_T('')
+from Binary_Op Import ComposeVector_RPY_T
+ComposeVector_RPY_T('')
+from binary_op import Multiply_vector_double
+Multiply_vector_double('')
+from binary_op import Compose_RPY_and_T
+Compose_RPY_and_T('')
+from Binary_Op Import EndomorphismBasis
+EndomorphismBasis('')
+from binary_op import Multiply_vector_matrix
+Multiply_vector_matrix('')
+from binary_op import Multiply_vector_matrixHomo
+Multiply_vector_matrixHomo('')
+from Binary_Op Import ConvolutionTemporal
+ConvolutionTemporal('')
