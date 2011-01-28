76979eff0d2fb2d3a3890855d1ec7d112cba188f
Mansard <nmansard@laas.fr>
2011-01-28 14:44:47 +0100

    IVIGIT.

---
+++ src/dynamic_graph/sot/core/math_small_entities.py
@@ -0,0 +1,9 @@
+from derivator import Derivator_of_Matrix
+Derivator_of_Matrix('')
+from derivator import Derivator_of_Vector
+Derivator_of_Vector('')
+
+from vector_constant import VectorConstant
+VectorConstant('')
+from matrix_constant import MatrixConstant
+MatrixConstant('')
