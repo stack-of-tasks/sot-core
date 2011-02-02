67006ba21d6a8aeef48089fd95ec278f3f6bd2b6
Mansard <nmansard@laas.fr>
2011-02-02 16:31:34 +0100

    Reforge the implementation of the standards sot-core cast.

--- src/signal/signal-cast.cpp
+++ src/signal/signal-cast.cpp
@@ -20,101 +20,116 @@
 
 #include <dynamic-graph/signal-caster.h>
 #include <sot-core/pool.h>
-#include <sot-core/signal-cast.h>
 #include <iomanip>
-#include <sot-core/feature-abstract.h>
 
+#include <sot-core/feature-abstract.h>
 #include "sot-core/matrix-homogeneous.h"
 #include "sot-core/matrix-rotation.h"
+#include <sot-core/flags.h>
+#include <sot-core/multi-bound.h>
+#include <dynamic-graph/signal-caster.h>
+#include <dynamic-graph/signal-cast-helper.h>
 
-using namespace std;
-using namespace sot;
+/* Implements a set of caster/displayer for the main types of sot-core. */
 
-FeatureAbstract* SignalCast<FeatureAbstract*>::
-cast( std::istringstream& iss )
+
+namespace dynamicgraph
 {
-  FeatureAbstract* ref;
-  std::string name; iss >> name;
-  if( name.length())
-    {
-      ref = &sotPool.getFeature(name);
-    }
-  else { ref = NULL; }
-  return ref;
-}
+  using namespace std;
+  using namespace sot;
 
-void SignalCast<FeatureAbstract*>::
-disp( FeatureAbstract* t,std::ostream& os )
-{
-  if( t ) { t->display(os); os<<std::endl; }
-  else { os << "NULL" << std::endl; }
-}
+  /* --- CASTER IMPLEMENTATION ------------------------------------------------ */
+  /* --- CASTER IMPLEMENTATION ------------------------------------------------ */
+  /* --- CASTER IMPLEMENTATION ------------------------------------------------ */
+
+  DG_SIGNAL_CAST_DEFINITION(sot::Flags);
+  DG_ADD_CASTER(sot::Flags,flags);
+
+  namespace {
+
+    dynamicgraph::DefaultCastRegisterer <sot::MatrixHomogeneous>
+    matrixHomegeneousCastRegisterer;
+
+    dynamicgraph::DefaultCastRegisterer <sot::MatrixRotation>
+    matrixRotationCastRegisterer;
+  }
+  /* --- FEATURE ABSTRACT ----------------------------------------------------- */
+  /* --- FEATURE ABSTRACT ----------------------------------------------------- */
+  /* --- FEATURE ABSTRACT ----------------------------------------------------- */
+
+  DG_SIGNAL_CAST_DEFINITION_HPP(sot::FeatureAbstract*);
+
+  FeatureAbstract* SignalCast<FeatureAbstract*>::
+  cast( std::istringstream& iss )
+  {
+    FeatureAbstract* ref;
+    std::string name; iss >> name;
+    if( name.length())
+      {
+	ref = &sotPool.getFeature(name);
+      }
+    else { ref = NULL; }
+    return ref;
+  }
+
+  void SignalCast<FeatureAbstract*>::
+  disp( FeatureAbstract* const& t,std::ostream& os )
+  {
+    if( t ) { t->display(os); os<<std::endl; }
+    else { os << "NULL" << std::endl; }
+  }
+
+  DG_ADD_CASTER(FeatureAbstract*,FAptr);
 
 
+  /* --- TIMEVAL -------------------------------------------------------------- */
+  /* --- TIMEVAL -------------------------------------------------------------- */
+  /* --- TIMEVAL -------------------------------------------------------------- */
+  DG_SIGNAL_CAST_DEFINITION_HPP(struct timeval);
 
-struct timeval SignalCast<struct timeval>::
-cast( std::istringstream& iss )
-{
-  int u,s; iss >> s >> u;
-  struct timeval t; t.tv_sec = s; t.tv_usec = u;
-  return t;
-}
-void SignalCast<struct timeval>::
-disp( const struct timeval& t,std::ostream& os )
-{
-  os << t.tv_sec << "s "<< t.tv_usec << "ms";
-}
+  struct timeval SignalCast<struct timeval>::
+    cast( std::istringstream& iss )
+  {
+    int u,s; iss >> s >> u;
+    struct timeval t; t.tv_sec = s; t.tv_usec = u;
+    return t;
+  }
+  void SignalCast<struct timeval>::
+  disp( const struct timeval& t,std::ostream& os )
+  {
+    os << t.tv_sec << "s "<< t.tv_usec << "ms";
+  }
 
-void SignalCast<VectorMultiBound>::
-trace( const VectorMultiBound& t,std::ostream& os )
-{
-  for( VectorMultiBound::const_iterator iter=t.begin();t.end()!=iter;++iter )
-    {
-      switch( iter->mode )
-        {
-        case MultiBound::MODE_SINGLE:
-          os << iter->getSingleBound() << "\t";
-          break;
-        case MultiBound::MODE_DOUBLE:
-          if( iter->getDoubleBoundSetup(MultiBound::BOUND_INF) )
-            os << iter->getDoubleBound(MultiBound::BOUND_INF)<<"\t";
-          else os <<"-inf\t";
-          if( iter->getDoubleBoundSetup(MultiBound::BOUND_SUP) )
-            os << iter->getDoubleBound(MultiBound::BOUND_SUP)<<"\t";
-          else os <<"+inf\t";
-          break;
-        }
-    }
-}
+  DG_ADD_CASTER(struct timeval,tv);
 
-namespace {
-  dynamicgraph::SignalCastRegisterer sotCastRegisterer_FeatureAbstractPtr
-  (typeid(FeatureAbstract*),
-   SignalCast<FeatureAbstract*>::disp_,
-   SignalCast<FeatureAbstract*>::cast_,
-   SignalCast<FeatureAbstract*>::trace_);
+  /* --- MULTI BOUND ---------------------------------------------------------- */
+  /* --- MULTI BOUND ---------------------------------------------------------- */
+  /* --- MULTI BOUND ---------------------------------------------------------- */
 
-  dynamicgraph::SignalCastRegisterer sotCastRegisterer_Flags
-    (typeid(Flags),
-     SignalCast<Flags>::disp_,
-     SignalCast<Flags>::cast_,
-     SignalCast<Flags>::trace_);
+  DG_SIGNAL_CAST_DEFINITION_TRACE(sot::VectorMultiBound);
 
-  dynamicgraph::SignalCastRegisterer sotCastRegisterer_VectorMultiBound
-    (typeid(VectorMultiBound),
-     SignalCast<VectorMultiBound>::disp_,
-     SignalCast<VectorMultiBound>::cast_,
-     SignalCast<VectorMultiBound>::trace_);
+  void SignalCast<VectorMultiBound>::
+  trace( const VectorMultiBound& t,std::ostream& os )
+  {
+    for( VectorMultiBound::const_iterator iter=t.begin();t.end()!=iter;++iter )
+      {
+	switch( iter->mode )
+	  {
+	  case MultiBound::MODE_SINGLE:
+	    os << iter->getSingleBound() << "\t";
+	    break;
+	  case MultiBound::MODE_DOUBLE:
+	    if( iter->getDoubleBoundSetup(MultiBound::BOUND_INF) )
+	      os << iter->getDoubleBound(MultiBound::BOUND_INF)<<"\t";
+	    else os <<"-inf\t";
+	    if( iter->getDoubleBoundSetup(MultiBound::BOUND_SUP) )
+	      os << iter->getDoubleBound(MultiBound::BOUND_SUP)<<"\t";
+	    else os <<"+inf\t";
+	    break;
+	  }
+      }
+  }
+  DG_ADD_CASTER(sot::VectorMultiBound,sotVMB);
 
-  dynamicgraph::SignalCastRegisterer sotCastRegisterer_timeval
-    (typeid(timeval),
-     SignalCast<timeval>::disp_,
-     SignalCast<timeval>::cast_,
-     SignalCast<timeval>::trace_);
 
-  dynamicgraph::DefaultCastRegisterer <sot::MatrixHomogeneous>
-  matrixHomegeneousCastRegisterer;
-
-  dynamicgraph::DefaultCastRegisterer <sot::MatrixRotation>
-  matrixRotationCastRegisterer;
-}
+}// namespace dynamicgraph
