3b98a28d935dca942e739f1d9135565c1c0649a5
Thomas Moulard <thomas.moulard@gmail.com>
2011-02-15 16:04:30 +0100

    Export FeatureVisualPoint entity in Python.

--- src/dynamic_graph/sot/core/__init__.py
+++ src/dynamic_graph/sot/core/__init__.py
@@ -5,6 +5,7 @@ from feature_position import FeaturePosition
 from feature_point6d_relative import FeaturePoint6dRelative
 from feature_generic import FeatureGeneric
 from feature_joint_limits import FeatureJointLimits
+from feature_visual_point import FeatureVisualPoint
 from binary_op import Compose_R_and_T
 from task import Task
 from constraint import Constraint
@@ -18,6 +19,7 @@ FeaturePoint6d('')
 FeaturePoint6dRelative('')
 FeatureGeneric('')
 FeatureJointLimits('')
+FeatureVisualPoint('')
 Compose_R_and_T('')
 Task('')
 Constraint('')
