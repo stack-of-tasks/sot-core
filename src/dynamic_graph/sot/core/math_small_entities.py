9871490123894ad2825a481f5fd170ae521f89f4
Florent Lamiraux <florent@laas.fr>
2011-06-21 00:05:52 +0200

    Merge unary-op and binary op in the same plug-in
        The double definition of some classes implies double free error at end
        of session.

--- src/dynamic_graph/sot/core/math_small_entities.py
+++ src/dynamic_graph/sot/core/math_small_entities.py
@@ -9,106 +9,106 @@ from matrix_constant import MatrixConstant
 MatrixConstant('')
 
 
-from unary_op import Inverse_of_matrix
+from operator import Inverse_of_matrix
 Inverse_of_matrix('')
-from unary_op import Inverse_of_matrixHomo
+from operator import Inverse_of_matrixHomo
 Inverse_of_matrixHomo('')
-from unary_op import Inverse_of_matrixtwist
+from operator import Inverse_of_matrixtwist
 Inverse_of_matrixtwist('')
-from unary_op import Inverse_of_matrixrotation
+from operator import Inverse_of_matrixrotation
 Inverse_of_matrixrotation('')
-from unary_op import Inverse_of_unitquat
+from operator import Inverse_of_unitquat
 Inverse_of_unitquat('')
-from unary_op import Selec_of_vector
+from operator import Selec_of_vector
 Selec_of_vector('')
-from unary_op import Selec_of_matrix
+from operator import Selec_of_matrix
 Selec_of_matrix('')
-from unary_op import Selec_column_of_matrix
+from operator import Selec_column_of_matrix
 Selec_column_of_matrix('')
-from unary_op import MatrixHomoToPoseUTheta
+from operator import MatrixHomoToPoseUTheta
 MatrixHomoToPoseUTheta('')
-from unary_op import SkewSymToVector
+from operator import SkewSymToVector
 SkewSymToVector('')
-from unary_op import PoseUThetaToMatrixHomo
+from operator import PoseUThetaToMatrixHomo
 PoseUThetaToMatrixHomo('')
-from unary_op import MatrixHomoToPoseQuaternion
+from operator import MatrixHomoToPoseQuaternion
 MatrixHomoToPoseQuaternion('')
-from unary_op import MatrixHomoToPoseRollPitchYaw
+from operator import MatrixHomoToPoseRollPitchYaw
 MatrixHomoToPoseRollPitchYaw('')
-from unary_op import PoseRollPitchYawToMatrixHomo
+from operator import PoseRollPitchYawToMatrixHomo
 PoseRollPitchYawToMatrixHomo('')
-from unary_op import PoseRollPitchYawToPoseUTheta
+from operator import PoseRollPitchYawToPoseUTheta
 PoseRollPitchYawToPoseUTheta('')
-from unary_op import HomoToMatrix
+from operator import HomoToMatrix
 HomoToMatrix('')
-from unary_op import MatrixToHomo
+from operator import MatrixToHomo
 MatrixToHomo('')
-from unary_op import HomoToTwist
+from operator import HomoToTwist
 HomoToTwist('')
-from unary_op import HomoToRotation
+from operator import HomoToRotation
 HomoToRotation('')
-from unary_op import RPYToMatrix
+from operator import RPYToMatrix
 RPYToMatrix('')
-from unary_op import MatrixToRPY
+from operator import MatrixToRPY
 MatrixToRPY('')
-from unary_op import QuaternionToMatrix
+from operator import QuaternionToMatrix
 QuaternionToMatrix('')
-from unary_op import MatrixToQuaternion
+from operator import MatrixToQuaternion
 MatrixToQuaternion('')
-from unary_op import MatrixToUTheta
+from operator import MatrixToUTheta
 MatrixToUTheta('')
-from unary_op import UThetaToQuaternion
+from operator import UThetaToQuaternion
 UThetaToQuaternion('')
-from unary_op import MatrixDiagonal
+from operator import MatrixDiagonal
 MatrixDiagonal('')
 
 
-from binary_op import Add_of_matrix
+from operator import Add_of_matrix
 Add_of_matrix('')
-from binary_op import Add_of_double
+from operator import Add_of_double
 Add_of_double('')
-from binary_op import Multiply_of_vector
+from operator import Multiply_of_vector
 Multiply_of_vector('')
-from binary_op import Multiply_of_matrix
+from operator import Multiply_of_matrix
 Multiply_of_matrix('')
-from binary_op import Multiply_of_matrixHomo
+from operator import Multiply_of_matrixHomo
 Multiply_of_matrixHomo('')
-from binary_op import Multiply_of_matrixrotation
+from operator import Multiply_of_matrixrotation
 Multiply_of_matrixrotation('')
-from binary_op import Multiply_of_matrixtwist
+from operator import Multiply_of_matrixtwist
 Multiply_of_matrixtwist('')
-from binary_op import Multiply_of_quaternion
+from operator import Multiply_of_quaternion
 Multiply_of_quaternion('')
-from binary_op import Multiply_of_double
+from operator import Multiply_of_double
 Multiply_of_double('')
-from binary_op import Substract_of_vector
+from operator import Substract_of_vector
 Substract_of_vector('')
-from binary_op import Substract_of_matrix
+from operator import Substract_of_matrix
 Substract_of_matrix('')
-from binary_op import Substract_of_double
+from operator import Substract_of_double
 Substract_of_double('')
-from binary_op import Stack_of_vector
+from operator import Stack_of_vector
 Stack_of_vector('')
-#from binary_op import WeightAdd_of_vector
+#from operator import WeightAdd_of_vector
 #WeightAdd_of_vector('')
-#from binary_op import WeightDir
+#from operator import WeightDir
 #WeightDir('')
-#from binary_op import Nullificator
+#from operator import Nullificator
 #Nullificator('')
-from binary_op import Compose_R_and_T
+from operator import Compose_R_and_T
 Compose_R_and_T('')
-#from binary_op import ComposeVector_RPY_T
+#from operator import ComposeVector_RPY_T
 #ComposeVector_RPY_T('')
-from binary_op import Multiply_double_vector
+from operator import Multiply_double_vector
 Multiply_double_vector('')
-from binary_op import Multiply_matrix_vector
+from operator import Multiply_matrix_vector
 Multiply_matrix_vector('')
-from binary_op import Multiply_matrixHomo_vector
+from operator import Multiply_matrixHomo_vector
 Multiply_matrixHomo_vector('')
-#from binary_op import Compose_RPY_and_T
+#from operator import Compose_RPY_and_T
 #Compose_RPY_and_T('')
-#from binary_op import EndomorphismBasis
+#from operator import EndomorphismBasis
 #EndomorphismBasis('')
-from binary_op import ConvolutionTemporal
+from operator import ConvolutionTemporal
 ConvolutionTemporal('')
 
