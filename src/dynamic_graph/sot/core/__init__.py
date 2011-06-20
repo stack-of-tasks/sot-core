af950a0e8a4ebeca3d7aeb03f6777c602aa55d31
Florent Lamiraux <florent@laas.fr>
2011-06-20 22:30:19 +0200

    Do not import unary-op and binary-op at python module initialization
        These two modules are incompatible in the sense that if both are imported,
        there is a double free error at exit. This might be due to the double
        definition of some template classes.

--- src/dynamic_graph/sot/core/__init__.py
+++ src/dynamic_graph/sot/core/__init__.py
@@ -1,5 +1,4 @@
 from robot_simu import RobotSimu
-from unary_op import RPYToMatrix, MatrixHomoToPoseRollPitchYaw
 from feature_point6d import FeaturePoint6d
 from feature_position import FeaturePosition
 from feature_posture import FeaturePosture
@@ -7,7 +6,6 @@ from feature_point6d_relative import FeaturePoint6dRelative
 from feature_generic import FeatureGeneric
 from feature_joint_limits import FeatureJointLimits
 from feature_visual_point import FeatureVisualPoint
-from binary_op import Compose_R_and_T
 from task import Task
 from task_pd import TaskPD
 from constraint import Constraint
@@ -16,15 +14,12 @@ from joint_limitator import JointLimitator
 from sot import SOT
 
 RobotSimu('')
-RPYToMatrix('')
-MatrixHomoToPoseRollPitchYaw('')
 FeaturePoint6d('')
 FeaturePosture('')
 FeaturePoint6dRelative('')
 FeatureGeneric('')
 FeatureJointLimits('')
 FeatureVisualPoint('')
-Compose_R_and_T('')
 Task('')
 TaskPD('')
 Constraint('')
