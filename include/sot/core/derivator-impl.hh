e21c5ef415e6cadced9e79d29968deff57ca3f2b
Francois Keith <francois.keith@inrialpes.fr>
2011-07-09 20:34:14 +0200

    Win32: force the creation of a library for the class derivator
    To this purpose, add a specification for the class derivator.
    For unix systems, this only comes down to creating a typename.
    For win32 systems, the idea is to create a class that will
     inherit the specification and be exported.

---
+++ include/sot/core/derivator-impl.hh
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2010,
+ * François Bleibel,
+ * Olivier Stasse,
+ *
+ * CNRS/AIST
+ *
+ * This file is part of sot-core.
+ * sot-core is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * sot-core is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.  You should
+ * have received a copy of the GNU Lesser General Public License along
+ * with sot-core.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SOT_DERIVATOR_IMPL_H__
+#define __SOT_DERIVATOR_IMPL_H__
+
+#include <sot/core/derivator.hh>
+#include <sot/core/vector-quaternion.hh>
+
+/* --------------------------------------------------------------------- */
+/* --- API ------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+#if defined (WIN32) 
+#  if defined (derivator_EXPORTS)
+#    define DERIVATOR_EXPORT __declspec(dllexport)
+#  else  
+#    define DERIVATOR_EXPORT  __declspec(dllimport)
+#  endif 
+#else
+#  define DERIVATOR_EXPORT 
+#endif
+
+/* --------------------------------------------------------------------- */
+/* --- INCLUDE --------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+namespace dynamicgraph { namespace sot {
+namespace dg = dynamicgraph;
+
+#ifdef WIN32
+# define DECLARE_SPECIFICATION(className, sotSigType )            \
+   class DERIVATOR_EXPORT className : public Derivator<sotSigType>  \
+   {                                                                \
+ public:                                                           \
+   className( const std::string& name );                            \
+   };
+#else
+# define DECLARE_SPECIFICATION(, sotSigType) \
+   typedef Derivator<sotSigType,sotCoefType> className;
+#endif
+
+DECLARE_SPECIFICATION(DerivatorDouble,double)
+DECLARE_SPECIFICATION(DerivatorVector,ml::Vector)
+DECLARE_SPECIFICATION(DerivatorMatrix,ml::Matrix)
+DECLARE_SPECIFICATION(DerivatorVectorQuaternion,VectorQuaternion)
+} /* namespace sot */} /* namespace dynamicgraph */
+
+
+
+#endif // #ifndef __SOT_DERIVATOR_H__
