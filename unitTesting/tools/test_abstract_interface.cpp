c9706fa85788cf67e092998f7e410f6e98884238
Olivier Stasse <olivier.stasse@gmail.com>
2011-12-22 16:58:12 +0100

    Add boost-options to specify robot-controller library for testing purposes.
    test_abstract_interface takes a library as an input, and try to load
    the whole control architecture for a specific robot controller.
    For instance for hrp-2 it is:
    ./test_abstract_interface --input-file libsot-hrp2-14-controller.so
    
    You need to make sure that our environment knows how to find
    libsot-hrp2-14-controller.so.

--- unitTesting/tools/test_abstract_interface.cpp
+++ unitTesting/tools/test_abstract_interface.cpp
@@ -25,26 +25,58 @@
 // POSIX.1-2001
 #include <dlfcn.h>
 
+#include <boost/program_options.hpp>
+
 #include "plugin.hh"
 #include <sot/core/debug.hh>
 
 using namespace std;
 using namespace dynamicgraph::sot; 
+namespace po = boost::program_options;
 
 class PluginLoader {
 
 protected:
   PluginAbstract * sotController_;
-  
+  po::variables_map vm_;
+  std::string dynamicLibraryName_;
+
 public:
   PluginLoader() {};
   ~PluginLoader() {};
+
+  int parseOptions(int argc, char *argv[])
+  { po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help", "produce help message")
+      ("input-file", po::value<string>(), "library to load")
+      ;
+    
+
+    po::store(po::parse_command_line(argc, argv, desc), vm_);
+    po::notify(vm_);    
+    
+    if (vm_.count("help")) {
+      cout << desc << "\n";
+      return -1;
+    }
+    if (!vm_.count("input-file")) {
+      cout << "No filename specified\n";
+      return -1;
+    }
+    else
+      dynamicLibraryName_= vm_["input-file"].as< string >();
+
+    Initialization();
+    return 0;
+  }
   
-  void Initialization(std::string &dynamicLibraryName)
+  void Initialization()
   {
+
     // Load the SotRobotBipedController library.
-    void * SotRobotControllerLibrary = dlopen(dynamicLibraryName.c_str(),
-					      RTLD_LAZY | RTLD_LOCAL );
+    void * SotRobotControllerLibrary = dlopen( "libpluginabstract.so",
+					       RTLD_LAZY | RTLD_LOCAL );
     if (!SotRobotControllerLibrary) {
       std::cerr << "Cannot load library: " << dlerror() << '\n';
       return ;
@@ -65,15 +97,14 @@ public:
     
     // Create robot-controller
     sotController_ = createPlugin();
-    std::string s="libsot-hrp2-14-controller.so";
-    sotController_->Initialization(s);
+    //    std::string s="libsot-hrp2-14-controller.so";
+    sotController_->Initialization(dynamicLibraryName_);
     cout <<"Went out from Initialization." << endl;
   }
 };
 
-int main(void)
+int main(int argc, char *argv[])
 {
   PluginLoader aPluginLoader;
-  string soPluginAbstract("libpluginabstract.so");
-  aPluginLoader.Initialization(soPluginAbstract);
+  aPluginLoader.parseOptions(argc,argv);
 }
