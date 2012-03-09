a2f9f3779c08d2266571f184d0433fcee3a84a7f
Florent Lamiraux <florent@laas.fr>
2012-03-09 13:43:22 +0100

    Remove patch aimed at ensuring backward compatibility in sot/core/__init__.py.

--- src/dynamic_graph/sot/core/__init__.py
+++ src/dynamic_graph/sot/core/__init__.py
@@ -12,42 +12,6 @@ from constraint import Constraint
 from gain_adaptive import GainAdaptive
 from joint_limitator import JointLimitator
 from sot import SOT
-#from smooth_reach import SmoothReach
-
-# --- PATCH --------------------------------------------------------------------
-'''
-This patch is used to keep the behavior of the sdes signal, while moving to the
-new formulation using the getter getReference and the setter setReference. This
-patch is temporary, and will be maintained during the time needed to convert
-all the depending application to the new syntaxt.
-'''
-
-class SdesPatch(object):
-    def __init__(self,feature):
-        self.feature = feature
-    @property
-    def value(self):
-        print 'Warning, deprecated: signal sdes for features is deprecated. Use the g/setReference functions instead.'
-        return self.feature.getReference()
-    @value.setter
-    def value(self,v):
-        print 'Warning, deprecated: signal sdes for features is deprecated. Use the g/setReference functions instead.'
-        self.feature.setReference(v)
-
-def patchFeature(FeatureClass):
-    FeatureClass.initWithoutSdesPatch = FeatureClass.__init__ 
-    def FeatureInitPatched(self,name):
-        FeatureClass.initWithoutSdesPatch(self,name)
-        self.sdes = SdesPatch(self)
-    FeatureClass.__init__ = FeatureInitPatched
-
-patchFeature(FeaturePoint6d)
-patchFeature(FeaturePosture)
-patchFeature(FeaturePoint6dRelative)
-patchFeature(FeatureGeneric)
-patchFeature(FeatureJointLimits)
-patchFeature(FeatureVisualPoint)
-# --- /PATCH -------------------------------------------------------------------
 
 RobotSimu('')
 FeaturePoint6d('')
