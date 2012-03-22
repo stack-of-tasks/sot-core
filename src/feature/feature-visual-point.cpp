955a927e740d12eb69eafb64d2876a6e39b7bfd7
Florent Lamiraux <florent@laas.fr>
2012-03-22 15:02:37 +0100

    Remouve output to standard output.

--- src/feature/feature-visual-point.cpp
+++ src/feature/feature-visual-point.cpp
@@ -104,9 +104,7 @@ computeJacobian( ml::Matrix& J,int time )
   const Flags &fl = selectionSIN(time);
 
   const int dim = dimensionSOUT(time);
-  std::cout<<" Dimension="<<dim<<std::endl;
   L.resize(dim,6) ;
-  std::cout<<L.matrix<<std::endl;
   unsigned int cursorL = 0;
 
   sotDEBUG(5)<<std::endl;
