fb3c9cec51410d5fdadf59b3cac046dd0528379e
Mansard <nmansard@laas.fr>
2012-07-28 16:10:21 +0200

    ivigit.

---
+++ src/dynamic_graph/sot/core/meta_tasks.py
@@ -0,0 +1,70 @@
+from dynamic_graph.sot.core.meta_task_6d import toFlags
+from dynamic_graph import plug
+from dynamic_graph.sot.core import *
+from dynamic_graph.sot.core.matrix_util import matrixToTuple, vectorToTuple,rotate
+from numpy import matrix, identity, zeros, eye, ndarray
+
+class MetaTaskCom(object):
+    def __init__(self,dyn,name="com"):
+        self.dyn=dyn
+        self.name=name
+        dyn.setProperty('ComputeCoM','true')
+
+        self.feature    = FeatureGeneric('feature'+name)
+        self.featureDes = FeatureGeneric('featureDes'+name)
+        self.gain = GainAdaptive('gain'+name)
+
+        plug(dyn.com,self.feature.errorIN)
+        plug(dyn.Jcom,self.feature.jacobianIN)
+        self.feature.setReference(self.featureDes.name)
+
+    def plugTask(self):
+        self.task.add(self.feature.name)
+        plug(self.task.error,self.gain.error)
+        plug(self.gain.gain,self.task.controlGain)
+
+    @property
+    def ref(self):
+        return self.featureDes.errorIN.value
+
+    @ref.setter
+    def ref(self,v):
+        self.featureDes.errorIN.value = v
+
+
+# --- HELPER FUNCTIONS ---------------------------------------------------------
+def setGain(gain,val):
+    if val!=None:
+        if isinstance(val,int) or isinstance(val,float):
+            gain.setConstant(val)
+        if len(val)==1:
+            gain.setConstant(val[0])
+        elif len(val)==3: gain.set( val[0],val[1],val[2])
+        elif len(val)==4: gain.setByPoint( val[0],val[1],val[2],val[3])
+
+def generic6dReference(p):
+    M=eye(4)
+    if isinstance(p,(matrix,ndarray)) and p.size == 3: M[0:3,3] = p
+    elif isinstance(p,tuple) and len(p) == 3:  M[0:3,3] = p
+    elif isinstance(p,(matrix,ndarray)) and p.shape == (4,4): M = p
+    else: print "Position with other parameters ... todo"
+    return M
+
+def goto6d(task,position,gain=None,resetJacobian=True):
+    M=generic6dReference(position)
+    task.featureDes.position.value = matrixToTuple(M)
+    task.feature.selec.value = "111111"
+    setGain(task.gain,gain)
+    if 'resetJacobianDerivative' in task.task.__class__.__dict__.keys() and resetJacobian:
+        task.task.resetJacobianDerivative()
+
+def gotoNd(task,position,selec,gain=None,resetJacobian=True):
+    M=generic6dReference(position)
+    if isinstance(selec,str):   task.feature.selec.value = selec
+    else: task.feature.selec.value = toFlags(selec)
+    task.featureDes.position.value = matrixToTuple(M)
+    setGain(task.gain,gain)
+    if 'resetJacobianDerivative' in task.task.__class__.__dict__.keys() and resetJacobian:
+        task.task.resetJacobianDerivative()
+
+
