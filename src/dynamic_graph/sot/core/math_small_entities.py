5260d6e1d447b81a7c17d68dac3cfac82864f5ef
Florent Lamiraux <florent@laas.fr>
2012-09-26 14:12:02 +0200

    Do not create entities at module importation.

--- src/dynamic_graph/sot/core/math_small_entities.py
+++ src/dynamic_graph/sot/core/math_small_entities.py
@@ -1,114 +1,58 @@
 from derivator import Derivator_of_Matrix
-Derivator_of_Matrix('')
 from derivator import Derivator_of_Vector
-Derivator_of_Vector('')
 
 from vector_constant import VectorConstant
-VectorConstant('')
 from matrix_constant import MatrixConstant
-MatrixConstant('')
-
 
 from operator import Inverse_of_matrix
-Inverse_of_matrix('')
 from operator import Inverse_of_matrixHomo
-Inverse_of_matrixHomo('')
 from operator import Inverse_of_matrixtwist
-Inverse_of_matrixtwist('')
 from operator import Inverse_of_matrixrotation
-Inverse_of_matrixrotation('')
 from operator import Inverse_of_unitquat
-Inverse_of_unitquat('')
 from operator import Selec_of_vector
-Selec_of_vector('')
 from operator import Selec_of_matrix
-Selec_of_matrix('')
 from operator import Selec_column_of_matrix
-Selec_column_of_matrix('')
 from operator import MatrixHomoToPoseUTheta
-MatrixHomoToPoseUTheta('')
 from operator import SkewSymToVector
-SkewSymToVector('')
 from operator import PoseUThetaToMatrixHomo
-PoseUThetaToMatrixHomo('')
 from operator import MatrixHomoToPoseQuaternion
-MatrixHomoToPoseQuaternion('')
 from operator import MatrixHomoToPoseRollPitchYaw
-MatrixHomoToPoseRollPitchYaw('')
 from operator import PoseRollPitchYawToMatrixHomo
-PoseRollPitchYawToMatrixHomo('')
 from operator import PoseRollPitchYawToPoseUTheta
-PoseRollPitchYawToPoseUTheta('')
 from operator import HomoToMatrix
-HomoToMatrix('')
 from operator import MatrixToHomo
-MatrixToHomo('')
 from operator import HomoToTwist
-HomoToTwist('')
 from operator import HomoToRotation
-HomoToRotation('')
 from operator import RPYToMatrix
-RPYToMatrix('')
 from operator import MatrixToRPY
-MatrixToRPY('')
 from operator import QuaternionToMatrix
-QuaternionToMatrix('')
 from operator import MatrixToQuaternion
-MatrixToQuaternion('')
 from operator import MatrixToUTheta
-MatrixToUTheta('')
 from operator import UThetaToQuaternion
-UThetaToQuaternion('')
 from operator import MatrixDiagonal
-MatrixDiagonal('')
-
 
 from operator import Add_of_matrix
-Add_of_matrix('')
 from operator import Add_of_double
-Add_of_double('')
 from operator import Multiply_of_vector
-Multiply_of_vector('')
 from operator import Multiply_of_matrix
-Multiply_of_matrix('')
 from operator import Multiply_of_matrixHomo
-Multiply_of_matrixHomo('')
 from operator import Multiply_of_matrixrotation
-Multiply_of_matrixrotation('')
 from operator import Multiply_of_matrixtwist
-Multiply_of_matrixtwist('')
 from operator import Multiply_of_quaternion
-Multiply_of_quaternion('')
 from operator import Multiply_of_double
-Multiply_of_double('')
 from operator import Substract_of_vector
-Substract_of_vector('')
 from operator import Substract_of_matrix
-Substract_of_matrix('')
 from operator import Substract_of_double
-Substract_of_double('')
 from operator import Stack_of_vector
-Stack_of_vector('')
 #from operator import WeightAdd_of_vector
-#WeightAdd_of_vector('')
 #from operator import WeightDir
-#WeightDir('')
 #from operator import Nullificator
-#Nullificator('')
 from operator import Compose_R_and_T
-Compose_R_and_T('')
 #from operator import ComposeVector_RPY_T
-#ComposeVector_RPY_T('')
 from operator import Multiply_double_vector
-Multiply_double_vector('')
 from operator import Multiply_matrix_vector
-Multiply_matrix_vector('')
 from operator import Multiply_matrixHomo_vector
-Multiply_matrixHomo_vector('')
 #from operator import Compose_RPY_and_T
-#Compose_RPY_and_T('')
 #from operator import EndomorphismBasis
-#EndomorphismBasis('')
 from operator import ConvolutionTemporal
-ConvolutionTemporal('')
 
