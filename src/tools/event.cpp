908a484de9ba6351c657843092c363cb072c1916
Joseph Mirabel <jmirabel@laas.fr>
2018-10-16 12:16:39 +0200

    [Minor] Move Event::check to definition file

--- src/tools/event.cpp
+++ src/tools/event.cpp
@@ -20,6 +20,23 @@
 
 namespace dynamicgraph {
   namespace sot {
+
+    bool& Event::check (bool& ret, const int& time)
+    {
+      const bool& val = conditionSIN (time);
+      ret = (val != lastVal_);
+      bool trigger = onlyUp_ ? (!lastVal_ && val) : ret;
+      if (ret) {
+        lastVal_ = val;
+        if (trigger) {
+          for (Triggers_t::const_iterator _s = triggers.begin();
+              _s != triggers.end(); ++_s)
+            (*_s)->recompute (time);
+        }
+      }
+      return ret;
+    }
+
     DYNAMICGRAPH_FACTORY_ENTITY_PLUGIN (Event, "Event");
   } // namespace sot
 } // namespace dynamicgraph
