f26cd09b9844fb2f1b3bd24407cf34519f43d4a4
Olivier Stasse <ostasse@laas.fr>
2019-10-24 15:30:18 +0200

    Fix test_device.cpp according to comments.

--- unitTesting/tools/test_device.cpp
+++ unitTesting/tools/test_device.cpp
@@ -9,7 +9,6 @@
 #include <iostream>
 #include <sot/core/debug.hh>
 
-
 #ifndef WIN32
 #include <unistd.h>
 #endif
@@ -17,70 +16,53 @@
 using namespace std;
 
 #include <boost/test/unit_test.hpp>
-#include <dynamic-graph/factory.h>
 #include <dynamic-graph/entity.h>
+#include <dynamic-graph/factory.h>
 #include <sot/core/device.hh>
 #include <sstream>
 
 using namespace dynamicgraph;
 using namespace dynamicgraph::sot;
 
-
-class TestDevice: public dg::sot::Device
-{
- public:
-  TestDevice(const std::string & RobotName):
-      Device(RobotName)
-  {
-    timestep_=0.001;
+class TestDevice : public dg::sot::Device {
+public:
+  TestDevice(const std::string &RobotName) : Device(RobotName) {
+    timestep_ = 0.001;
   }
-  ~TestDevice()
-  {
-  }
-  
+  ~TestDevice() {}
 };
-  
-BOOST_AUTO_TEST_CASE(test_device)
-{
+
+BOOST_AUTO_TEST_CASE(test_device) {
   TestDevice aDevice(std::string("simple_humanoid"));
 
   /// Fix constant vector for the control entry in position
   dg::Vector aStateVector(38);
   dg::Vector aVelocityVector(38);
-  dg::Vector aLowerVelBound(38),anUpperVelBound(38);
+  dg::Vector aLowerVelBound(38), anUpperVelBound(38);
   dg::Vector anAccelerationVector(38);
-  std::ostringstream ossControlVector;
-  std::istringstream issControlVector;
+  dg::Vector aControlVector(38);
 
-
-  ossControlVector<<"[38](";
   for (unsigned int i = 0; i < 38; i++) {
-    aLowerVelBound[i]= -3.14;
+    aLowerVelBound[i] = -3.14;
     aStateVector[i] = -0.5;
-    anUpperVelBound[i]= 3.14;
-    ossControlVector << "-0.1";
-    if (i<37)
-      ossControlVector << ",";
+    anUpperVelBound[i] = 3.14;
+    aControlVector(i)=-0.1;
   }
-  ossControlVector << ")";
-  issControlVector.str(ossControlVector.str());
+  
   aDevice.setVelocitySize(38);
-  aDevice.setVelocityBounds(aLowerVelBound,anUpperVelBound);
+  aDevice.setVelocityBounds(aLowerVelBound, anUpperVelBound);
   aDevice.setVelocity(aStateVector);
   aDevice.setState(aStateVector); // entry signal in position
-  aDevice.controlSIN.set(issControlVector);
-      
-  for (unsigned int i = 0; i < 2000; i++)
-  {
+  aDevice.controlSIN.setConstant(aControlVector);
+
+  for (unsigned int i = 0; i < 2000; i++) {
     aDevice.stateSOUT.recompute(i);
-    if (i==1)
+    if (i == 1)
       std::cout << " First integration " << aDevice.stateSOUT << std::endl;
-        
   }
 
   aDevice.display(std::cout);
   aDevice.cmdDisplay();
-  const dg::Vector & aControl = aDevice.motorcontrolSOUT(2001);
-  double diff = 0, ldiff;
-
+  // const dg::Vector &aControl = aDevice.motorcontrolSOUT(2001);
+  //  double diff = 0, ldiff;
 }
