3b29ac4eb9443ea4ca47b3a5497481357ad4e0ee
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-10-29 18:16:08 +0100

    [Python 3] fix imports

--- src/dynamic_graph/sot/core/math_small_entities.py
+++ src/dynamic_graph/sot/core/math_small_entities.py
@@ -5,7 +5,7 @@
 # from operator import WeightAdd_of_vector
 # from operator import WeightDir
 # from operator import Nullificator
-from operator import (
+from .operator import (
     Add_of_double, Add_of_matrix, Add_of_vector, Component_of_vector, Compose_R_and_T, ConvolutionTemporal,
     HomoToMatrix, HomoToRotation, HomoToTwist, Inverse_of_matrix, Inverse_of_matrixHomo, Inverse_of_matrixrotation,
     Inverse_of_matrixtwist, Inverse_of_unitquat, MatrixDiagonal, MatrixHomoToPose, MatrixHomoToPoseQuaternion,
@@ -17,6 +17,6 @@ from operator import (
     Selec_of_matrix, Selec_of_vector, SkewSymToVector, Stack_of_vector, Substract_of_double, Substract_of_matrix,
     Substract_of_vector, UThetaToQuaternion)
 
-from derivator import Derivator_of_Matrix, Derivator_of_Vector
-from matrix_constant import MatrixConstant
-from vector_constant import VectorConstant
+from .derivator import Derivator_of_Matrix, Derivator_of_Vector
+from .matrix_constant import MatrixConstant
+from .vector_constant import VectorConstant
