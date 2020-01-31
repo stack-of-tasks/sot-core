2a0005aeb80cbdf117523f4f884fc20d0a79b193
Joseph Mirabel <jmirabel@laas.fr>
2020-01-31 19:20:20 +0100

    Add Sot::maxControlIncrementSquaredNorm to avoid generating large controls.

--- include/sot/core/memory-task-sot.hh
+++ include/sot/core/memory-task-sot.hh
@@ -30,7 +30,7 @@ public: //   protected:
       KernelConst_t;
 
   /* Internal memory to reduce the dynamic allocation at task resolution. */
-  dg::Vector err, tmpTask, tmpVar;
+  dg::Vector err, tmpTask, tmpVar, tmpControl;
   dg::Matrix Jt; //( nJ,mJ );
 
   dg::Matrix JK; //(nJ,mJ);
