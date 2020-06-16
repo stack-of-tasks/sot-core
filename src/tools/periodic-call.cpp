64c2890c3f4c4f718c3ce6fa22778a8ca63bea97
Joseph Mirabel <jmirabel@laas.fr>
2020-06-16 15:49:27 +0200

    Remove PeriodicCall::addSpecificCommands

--- src/tools/periodic-call.cpp
+++ src/tools/periodic-call.cpp
@@ -112,60 +112,6 @@ static std::string readLineStr( istringstream& args )
   return res;
 }
 */
-#define ADD_COMMAND(name, def)                                                 \
-  if (commandMap.count(prefix + name) != 0) {                                  \
-    DG_THROW ExceptionFactory(ExceptionFactory::OBJECT_CONFLICT,               \
-                              "Command " + prefix + name +                     \
-                                  " already registered in Entity.");           \
-  }                                                                            \
-  commandMap.insert(std::make_pair(prefix + name, def))
-
-void PeriodicCall::addSpecificCommands(Entity &ent,
-                                       Entity::CommandMap_t &commandMap,
-                                       const std::string &prefix) {
-  using namespace dynamicgraph::command;
-
-  /* Explicit typage to help the compiler. */
-  boost::function<void(const std::string &)>
-      addSignal = boost::bind(&PeriodicCall::addSignal, this, _1),
-      rmSignal = boost::bind(&PeriodicCall::rmSignal, this, _1);
-  boost::function<void(const std::string &, const unsigned int &)>
-      addDownsampledSignal =
-          boost::bind(&PeriodicCall::addDownsampledSignal, this, _1, _2);
-  boost::function<void(void)> clear = boost::bind(&PeriodicCall::clear, this);
-  boost::function<void(std::ostream &)> disp =
-      boost::bind(&PeriodicCall::display, this, _1);
-
-  ADD_COMMAND(
-      "addSignal",
-      makeCommandVoid1(ent, addSignal,
-                       docCommandVoid1("Add the signal to the refresh list",
-                                       "string (sig name)")));
-  ADD_COMMAND("addDownsampledSignal",
-              makeCommandVoid2(
-                  ent, addDownsampledSignal,
-                  docCommandVoid2(
-                      "Add the signal to the refresh list", "string (sig name)",
-                      "unsigned int (downsampling factor, 1 means every time, "
-                      "2 means every other time, etc...")));
-  ADD_COMMAND(
-      "rmSignal",
-      makeCommandVoid1(ent, rmSignal,
-                       docCommandVoid1("Remove the signal to the refresh list",
-                                       "string (sig name)")));
-  ADD_COMMAND(
-      "clear",
-      makeCommandVoid0(
-          ent, clear,
-          docCommandVoid0(
-              "Clear all signals and commands from the refresh list.")));
-
-  ADD_COMMAND("disp",
-              makeCommandVerbose(
-                  ent, disp,
-                  docCommandVerbose(
-                      "Print the list of to-refresh signals and commands.")));
-}
 
 /*
  * Local variables:
