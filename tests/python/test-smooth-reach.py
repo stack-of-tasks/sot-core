0dca82c9ac9e7e0d29edc4f99712a21097f61c30
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-12-07 14:14:24 +0100

    [Tests] Smooth-Reach dumb test

---
+++ tests/python/test-smooth-reach.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import unittest
+from dynamic_graph.sot.core.smooth_reach import SmoothReach
+
+class SmoothReachTest(unittest.TestCase):
+    def test_smooth_reach(self):
+        sr = SmoothReach("banana")
+        self.assertIn("input(vector)::start (Type Cst)", str(sr.start))
+        self.assertIn("output(vector)::goal (Type Fun)", str(sr.goal))
+
+
+if __name__ == '__main__':
+    unittest.main()
