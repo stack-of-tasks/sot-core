a9af038cf70833993a1c7a0e0fe45f651bc8d2e2
Florent Lamiraux <florent@laas.fr>
2021-06-11 16:10:05 +0000

    Add and install fwd.hh header.

---
+++ include/sot/core/fwd.hh
@@ -0,0 +1,23 @@
+/*
+ * Copyright CNRS 2021
+ *
+ * Author: Florent Lamiraux
+ *
+ * This file is part of sot-core.
+ */
+
+#ifndef SOT_CORE_FWD_HH
+#define SOT_CORE_FWD_HH
+
+#include <dynamic-graph/fwd.hh>
+
+namespace dynamicgraph {
+namespace sot {
+
+class Device;
+class AbstractSotExternalInterface;
+  
+} // namespace sot
+}// namespace dynamicgraph
+
+#endif // SOT_CORE_FWD_HH
