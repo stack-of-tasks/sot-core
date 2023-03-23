a9c719f42a2769917c6b35904bb640ad831244dc
Florent Lamiraux <florent@laas.fr>
2023-03-23 12:16:04 +0100

    [Segment] New entity that extracts a subvector from a vector.

---
+++ include/sot/core/segment.hh
@@ -0,0 +1,62 @@
+// BSD 2-Clause License
+
+// Copyright (c) 2023, CNRS
+// Author: Florent Lamiraux
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in
+//   the documentation and/or other materials provided with the
+//   distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef SOT_CORE_SEGMENT_HH
+#define SOT_CORE_SEGMENT_HH
+
+#include <sot/core/config.hh>
+#include <dynamic-graph/entity.h>
+#include <dynamic-graph/signal-ptr.t.cpp>
+#include <dynamic-graph/signal-time-dependent.h>
+
+namespace dynamicgraph {
+namespace sot {
+  class SOT_CORE_DLLAPI Segment : public Entity
+  {
+  public:
+    static const std::string CLASS_NAME;
+    virtual const std::string& getClassName() const { return CLASS_NAME; }
+    // Set the range of input vector that is provided as output.
+    void setRange(const int& i0, const int& length)
+    {
+      range_ = std::make_pair(i0, length);
+    }
+
+    Segment(const std::string& name);
+  private:
+    Vector& compute(Vector& output, int time);
+    SignalPtr<Vector, int> inputSIN_;
+    SignalTimeDependent<Vector, int> outputSOUT_;
+    // Range of the input vector that is provided as output
+    std::pair<int,int> range_;
+  }; // class Segment
+} // namespace sot
+} // namespace dynamicgraph
+#endif //SOT_CORE_SEGMENT_HH
---
+++ src/math/segment.cpp
@@ -0,0 +1,65 @@
+// BSD 2-Clause License
+
+// Copyright (c) 2023, CNRS
+// Author: Florent Lamiraux
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in
+//   the documentation and/or other materials provided with the
+//   distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <iostream>
+#include <sot/core/segment.hh>
+#include <dynamic-graph/factory.h>
+#include <dynamic-graph/all-commands.h>
+
+namespace dynamicgraph {
+namespace sot {
+
+  DYNAMICGRAPH_FACTORY_ENTITY_PLUGIN(Segment, "Segment");
+
+  Segment::Segment(const std::string& name) :
+    Entity(name),
+    inputSIN_(0x0, "Segment(" + name + ")::input(vector)::in"),
+    outputSOUT_(boost::bind(&Segment::compute, this, _1, _2), inputSIN_,
+                "Segment(" + name + ")::out"),
+    range_(std::make_pair(0, 0))
+  {
+    using dynamicgraph::command::makeCommandVoid2;
+    signalRegistration(inputSIN_);
+    signalRegistration(outputSOUT_);
+    std::string docstring(
+        "Set range of input vector to be provided as output\n"
+        "  - input: first index, length\n");
+    addCommand("setRange", makeCommandVoid2(*this, &Segment::setRange,
+                                            docstring));
+  }
+
+  Vector& Segment::compute(Vector& output, int time)
+  {
+    const Vector& input(inputSIN_(time));
+    output = input.segment(std::get<0>(range_), std::get<1>(range_));
+    return output;
+  }
+} // namespace sot
+} // namespace dynamicgraph
