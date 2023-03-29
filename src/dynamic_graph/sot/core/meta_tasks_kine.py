5aff4c5f8cc734e0a4ec9f52cd3c7fc0e7a4b4a2
Florent Lamiraux <florent@laas.fr>
2023-03-29 11:31:50 +0200

    [FeaturePoint6d, FeaturePosition] Remove deprecated entities.

--- src/dynamic_graph/sot/core/meta_tasks_kine.py
+++ src/dynamic_graph/sot/core/meta_tasks_kine.py
@@ -1,34 +1,11 @@
 from dynamic_graph import plug
 from dynamic_graph.sot.core.gain_adaptive import GainAdaptive
-from dynamic_graph.sot.core.meta_task_6d import MetaTask6d
 
 # TODO: this function is imported from meta_tasks_kine in several places,
 # whereas it is defined in meta_tasks
 from dynamic_graph.sot.core.meta_tasks import gotoNd  # noqa
 from dynamic_graph.sot.core.meta_tasks import MetaTaskCom
 from dynamic_graph.sot.core.task import Task
-
-
-class MetaTaskKine6d(MetaTask6d):
-    def createTask(self):
-        self.task = Task("task" + self.name)
-
-    def createGain(self):
-        self.gain = GainAdaptive("gain" + self.name)
-        self.gain.set(0.1, 0.1, 125e3)
-
-    def plugEverything(self):
-        self.feature.setReference(self.featureDes.name)
-        plug(self.dyn.signal(self.opPoint), self.feature.signal("position"))
-        plug(self.dyn.signal("J" + self.opPoint), self.feature.signal("Jq"))
-        self.task.add(self.feature.name)
-        plug(self.task.error, self.gain.error)
-        plug(self.gain.gain, self.task.controlGain)
-
-    def keep(self):
-        self.feature.position.recompute(self.dyn.position.time)
-        self.feature.keep()
-
 
 class MetaTaskKineCom(MetaTaskCom):
     def __init__(self, dyn, name="com"):
