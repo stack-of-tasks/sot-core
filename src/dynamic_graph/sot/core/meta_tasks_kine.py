ede41b6ad81e444056d79fd534d410e6e107b569
Florent Lamiraux <florent@laas.fr>
2023-05-29 18:51:38 +0200

    [meta_task_kine] Do not import unused symbol.

--- src/dynamic_graph/sot/core/meta_tasks_kine.py
+++ src/dynamic_graph/sot/core/meta_tasks_kine.py
@@ -1,9 +1,3 @@
-from dynamic_graph import plug
-from dynamic_graph.sot.core.gain_adaptive import GainAdaptive
-
-# TODO: this function is imported from meta_tasks_kine in several places,
-# whereas it is defined in meta_tasks
-from dynamic_graph.sot.core.meta_tasks import gotoNd  # noqa
 from dynamic_graph.sot.core.meta_tasks import MetaTaskCom
 from dynamic_graph.sot.core.task import Task
 
