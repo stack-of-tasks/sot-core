4ba0037016b2813196fa976157faa604c33a53d3
Florent Lamiraux <florent@laas.fr>
2023-06-22 11:29:05 +0200

    [VectorToRotation] Remove deprecated entity.

--- src/matrix/vector-to-rotation-python.h
+++
@@ -1,7 +0,0 @@
-#include <sot/core/macros.hh>
-#include <sot/core/vector-to-rotation.hh>
-
-SOT_CORE_DISABLE_WARNING_PUSH
-SOT_CORE_DISABLE_WARNING_DEPRECATED
-typedef boost::mpl::vector<dynamicgraph::sot::VectorToRotation> entities_t;
-SOT_CORE_DISABLE_WARNING_POP
